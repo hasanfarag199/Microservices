f619a0c14fe2e72648da18a843b73797d5bb6542
Hasn <153518975+hasanfarag199@users.noreply.github.com>
2025-06-16 05:49:56 +0000

    working gke cluster

--- main.tf
+++ main.tf
@@ -8,39 +8,11 @@ terraform {
 }
 
 provider "google" {
-  # Configuration options
   project = "microservices-463006"
-  # terraform gcp service account key file
-  # This file should be downloaded from the GCP console and should not be committed to version control.
   credentials = "microservices-463006-621b97fb5092.json"
 }
-
-
-resource "google_container_cluster" "primary" {
-  name     = "my-gke-cluster"
+resource "google_container_cluster" "gke_cluster" {
+  name     = "mircoservices-gke-cluster"
   location = "us-central1"
-
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count       = 1
-}
-
-resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "my-node-pool"
-  location   = "us-central1"
-  cluster    = google_container_cluster.primary.name
-  node_count = 1
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-medium"
-
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account =  "terraform-gcp-service-account@cryptic-form-463004-u8.iam.gserviceaccount.com"
-    oauth_scopes    = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
+  initial_node_count = 1
 }
